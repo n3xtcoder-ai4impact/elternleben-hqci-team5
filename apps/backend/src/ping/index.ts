37f9ac8ad9bd88d425818966ee5afd5eaa8e8f0c
Tim Geisler <tg@webxcerpt.com>
2025-05-05 18:28:05 +0200

    chore: dummy debug code for triggering ping function

--- apps/backend/src/ping/index.ts
+++ apps/backend/src/ping/index.ts
@@ -1,6 +1,7 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+  console.log("Ping function triggered");
   context.res = {
     status: 200,
     body: 'Hello fron chat API.',
