66db4c62baf63daffff07926ff0f8ce2b2450403
Tim Geisler <tg@webxcerpt.com>
2025-05-08 22:57:42 +0200

    fix: styling

--- apps/frontend/src/app/ElternlebenChatBot.tsx
+++ apps/frontend/src/app/ElternlebenChatBot.tsx
@@ -15,8 +15,8 @@ export const ElternlebenChatBot = () => {
           headers: {
             'Content-Type': 'application/json',
           },
-          body: JSON.stringify({ userInput: params.userInput })
-				}
+          body: JSON.stringify({ userInput: params.userInput }),
+        }
       );
       const chatCompletion = await response.json();
       console.log(chatCompletion);
@@ -49,23 +49,80 @@ export const ElternlebenChatBot = () => {
   };
 
   const header = {
-    title: 'Elternleben Chatbot',
-    showAvatar: false,
+    title: 'Leni, der Elternleben-Chatbot',
+    showAvatar: true,
+    avatar: 'assets/Check_nobg_75.png',
     buttons: [],
   };
   const settings: Settings = {
-    general: { embedded: true },
-    chatHistory: { storageKey: 'example_llm_conversation' },
+    general: { embedded: true, showFooter: false },
+    chatInput: {
+      enabledPlaceholderText: 'Bitte gebe deine Frage ein ...',
+    },
+    chatHistory: {
+      disabled: true,
+      storageKey: 'example_llm_conversation',
+    },
     header,
+    botBubble: {
+      animate: true,
+      simStream: true,
+    },
+    chatWindow: {
+      showScrollbar: true,
+    },
+    emoji: {
+      disabled: true,
+    },
+    footer: {
+      buttons: [],
+    },
   };
 
   const elternlebenStyle = {
-    background: '#4CAF50',
-    color: '#fff',
+    background: 'rgb(85, 165, 50)',
+    color: 'rgb(255, 255, 255)',
+    fontFamily: '"Open Sans", Arial, sans-serif',
+    fontSize: '18px',
   };
   const styles: Styles = {
-    headerStyle: elternlebenStyle,
-    botBubbleStyle: elternlebenStyle,
+    bodyStyle: elternlebenStyle,
+
+    headerStyle: {
+      background: 'rgb(255, 255, 255)',
+      color: 'rgb(76, 76, 76)',
+      boxShadow: 'rgba(0, 0, 0, 0.3) 2px 2px 12px 0px',
+    },
+    botBubbleStyle: {
+      backgroundColor: 'rgb(237, 237, 237)',
+      color: 'rgb(76, 76, 76)',
+      borderRadius: '0px',
+      boxShadow: 'rgba(0, 0, 0, 0.3) 2px 2px 12px 0px',
+    },
+    userBubbleStyle: {
+      backgroundColor: 'rgb(255, 255, 255)',
+      color: 'rgb(76, 76, 76)',
+      borderRadius: '0px',
+      boxShadow: 'rgba(0, 0, 0, 0.3) 2px 2px 12px 0px',
+    },
+    sendButtonStyle: {
+      backgroundColor: 'rgb(244, 146, 5)',
+      borderRadius: '10px',
+      boxShadow: 'rgb(128, 128, 128) 5px 5px 5px 0px',
+    },
+    sendButtonDisabledStyle: {
+      backgroundColor: 'rgb(244, 146, 5)',
+      borderRadius: '10px',
+      boxShadow: 'rgb(128, 128, 128) 5px 5px 5px 0px',
+    },
+    sendButtonHoveredStyle: {
+      backgroundColor: 'rgb(244, 146, 5)',
+      borderRadius: '10px',
+      boxShadow: 'rgb(128, 128, 128) 5px 5px 5px 0px',
+    },
+    chatWindowStyle: {
+      borderRadius: '0px',
+    },
   };
 
   return <ChatBot settings={settings} styles={styles} flow={flow} />;
