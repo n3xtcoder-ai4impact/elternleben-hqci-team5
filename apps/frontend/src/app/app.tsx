fec580d3e5e10055316205328ac088734eeb3c72
Tim Geisler <tg@webxcerpt.com>
2025-05-19 11:34:44 +0200

    chore: WIP message

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -3,6 +3,7 @@ import { ElternlebenChatBot } from "./ElternlebenChatBot";
 export function App() {
   return (
     <div>
+      <h1>Work in progress! Chatbot might not work!</h1>
       <ElternlebenChatBot/>
     </div>
   );
