daa72795e82adf07a03179a403f79b75a62f35de
Tim Geisler <tg@webxcerpt.com>
2025-05-21 01:45:32 +0200

    fix: remove work in progress

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -3,7 +3,6 @@ import { ElternlebenChatBot } from "./ElternlebenChatBot";
 export function App() {
   return (
     <div>
-      <h1>Work in progress! Chatbot might not work!</h1>
       <ElternlebenChatBot/>
     </div>
   );
